f96a9740227d9ab65c55c732fe04334e973f8218
Sacha Corazzi <sacha@mobiusmedia.co.uk>
2016-02-28 17:20:44 +0000

    Finish basic page

---
+++ assets/js/app.js
@@ -0,0 +1,49 @@
+"use strict";
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var DreamCycle = (function () {
+    function DreamCycle() {
+        _classCallCheck(this, DreamCycle);
+    }
+
+    _createClass(DreamCycle, [{
+        key: 'init',
+        value: function init() {
+            if (typeof dreams === 'undefined') {
+                this.init();
+            } else {
+                this.showRandomDream();
+            }
+        }
+    }, {
+        key: 'showRandomDream',
+        value: function showRandomDream() {
+            var dream = dreams[Math.floor(Math.random() * dreams.length)];
+            this.showDream(dream);
+        }
+    }, {
+        key: 'showDream',
+        value: function showDream(dream) {
+
+            $('#date').text(dream.date);
+            $('#dream span').text(dream.dream);
+
+            $('#dream').fadeIn(3000, function () {
+                $('#date').fadeIn(1500);
+            });
+        }
+    }]);
+
+    return DreamCycle;
+})();
+
+var cycle = new DreamCycle();
+
+cycle.init();
+
+$('#random').on('click', function() {
+    window.location.reload();
+});
